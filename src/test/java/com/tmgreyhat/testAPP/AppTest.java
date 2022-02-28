f7909e7b835864a077c46268197f0b7ed88ee419
tapiwanasheMbizvo <tapiwambizvo@gmail.com>
2022-02-28 09:35:31 +0200

    initial commit

---
+++ src/test/java/com/tmgreyhat/testAPP/AppTest.java
@@ -0,0 +1,20 @@
+package com.tmgreyhat.testAPP;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
